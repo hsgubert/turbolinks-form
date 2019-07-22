7305e9de75955ea4db3e59f9b6aac223d11ba65b
Henrique Gubert <guberthenrique@hotmail.com>
2019-07-22 16:51:12 -0300

    Added polyfill for Element.prototype.matches()

--- app/assets/javascripts/turbolinks-form-core.js
+++ app/assets/javascripts/turbolinks-form-core.js
@@ -221,3 +221,20 @@ TurbolinksForm.on(document, "ajax:beforeSend", "[data-turbolinks-form]", functio
   // dispatches turbolinks event
   Turbolinks.dispatch('turbolinks:request-start', {data: {xhr: xhr}});
 });
+
+// Polyfills
+
+if (!Element.prototype.matches) {
+  Element.prototype.matches =
+      Element.prototype.matchesSelector ||
+      Element.prototype.mozMatchesSelector ||
+      Element.prototype.msMatchesSelector ||
+      Element.prototype.oMatchesSelector ||
+      Element.prototype.webkitMatchesSelector ||
+      function(s) {
+        var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+            i = matches.length;
+        while (--i >= 0 && matches.item(i) !== this) {}
+        return i > -1;
+      };
+}
